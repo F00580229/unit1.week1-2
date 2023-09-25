ffaf0369ffafad336675b22e785504bf133f83f4
F00580229 <145945366+F00580229@users.noreply.github.com>
2023-09-25 17:01:08 -0500

    Add files via upload

---
+++ First.js
@@ -0,0 +1,8 @@
+console.log("I am uploading a file in GitHub");
+
+const subMarks = [92, 95];  
+
+for (let i = 0; i < subMarks.length; i++) {
+  const marks = subMarks[i];  
+  console.log(`percentage of my in ${i + 1} semister: ${marks}`);
+}
